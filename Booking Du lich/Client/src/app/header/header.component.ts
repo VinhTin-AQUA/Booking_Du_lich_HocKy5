ae526226662c7b7e936200d99f742074f105f754
VinhTin-AQUA <tinhovinh@gmail.com>
2023-09-28 13:23:09 +0700

    fix conflict

--- Booking Du lich/Client/src/app/header/header.component.ts
+++ Booking Du lich/Client/src/app/header/header.component.ts
@@ -12,11 +12,7 @@ export class HeaderComponent {
     this.collapseMenu = !this.collapseMenu;
   }
 
-<<<<<<< HEAD
-  hideMenu() {
-=======
   hideMenu(){
->>>>>>> d20f87f637e0698958744b8d15f39bde110bd5a3
     this.collapseMenu = false;
   }
 }
