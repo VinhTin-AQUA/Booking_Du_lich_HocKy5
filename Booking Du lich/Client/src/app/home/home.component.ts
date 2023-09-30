f0c1243ba7d015dc64f47e3fe6ccf75b54454ed8
VinhTin-AQUA <tinhovinh@gmail.com>
2023-09-30 14:14:43 +0700

    banner in home

--- Booking Du lich/Client/src/app/home/home.component.ts
+++ Booking Du lich/Client/src/app/home/home.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+const today = new Date();
+const month = today.getMonth();
+const year = today.getFullYear();
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
 
+  rooms: number = 1;
+  people: number = 1;
+  isShowedDetailPeople: boolean = false;
+
+  show() {
+    console.log(JSON.stringify(this.range.value));
+  }
+
+  increaseRooms() {
+    this.rooms++;
+  }
+
+  decreaseRooms() {
+    this.rooms <= 1 ? (this.rooms = 1) : this.rooms--;
+  }
+
+  increasePeolple() {
+    this.people++;
+  }
+
+  decreasePeolple() {
+    this.people <= 1 ? (this.people = 1) : this.people--;
+  }
+
+  showDetailPeople() {
+    this.isShowedDetailPeople = !this.isShowedDetailPeople;
+  }
 }
