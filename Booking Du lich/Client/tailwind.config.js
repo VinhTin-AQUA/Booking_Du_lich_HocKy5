9b3f7555ed2d65f3941596a0e0a5540f8ef9003e
NguyenNgocTieuThu <nguyenngoctieuthu2207@gmail.com>
2023-09-30 13:57:37 +0700

    login_interface

--- Booking Du lich/Client/tailwind.config.js
+++ Booking Du lich/Client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "custom": '0 50px 25px -24px rgb(0 0 0 / 0.3)'
+      }
+    },
   },
   plugins: [],
 }
