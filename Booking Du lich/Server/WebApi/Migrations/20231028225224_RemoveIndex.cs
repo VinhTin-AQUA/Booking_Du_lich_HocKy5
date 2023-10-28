86539c6339321bb83ef794ffcf6cfd8a3e7e1216
7iendat <datnguyentien0803@gmail.com>
2023-10-29 06:09:41 +0700

    fix insert pk in roomprice and bookroom

---
+++ Booking Du lich/Server/WebApi/Migrations/20231028225224_RemoveIndex.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    public partial class RemoveIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CheckOutDate",
+                table: "Book Room",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.DropIndex(
+                name: "IX_Room Price_RoomId",
+                table: "Room Price");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CheckOutDate",
+                table: "Book Room",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+            migrationBuilder.DropIndex(
+                name: "IX_Room Price_RoomId",
+                table: "Room Price");
+        }
+    }
+}
