72ef94f2ed72936df1e50665eefa6046ba681fac
7iendat <datnguyentien0803@gmail.com>
2023-10-29 07:00:23 +0700

    update api hotel

--- Booking Du lich/Server/WebApi/DTOs/Hotel/AddHotel.cs
+++ Booking Du lich/Server/WebApi/DTOs/Hotel/AddHotel.cs
@@ -9,5 +9,11 @@ namespace WebApi.DTOs.Hotel
         [Display(Name = "Hotel name")]
         [Column(TypeName = "nvarchar(250)")]
         public string HotelName { get; set; }
+
+        public string PosterID { get; set; }
+
+        public string ApproverID { get; set; }
+        public DateTime? PostingDate { get; set; }
+        public DateTime? ApprovalDate { get; set; }
     }
 }
