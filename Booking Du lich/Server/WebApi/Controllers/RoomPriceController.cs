089e6afb5d99311e6ff883c775fcbd9f14707e52
7iendat <datnguyentien0803@gmail.com>
2023-10-30 21:14:38 +0700

    fix

--- Booking Du lich/Server/WebApi/Controllers/RoomPriceController.cs
+++ Booking Du lich/Server/WebApi/Controllers/RoomPriceController.cs
@@ -68,9 +68,9 @@ namespace WebApi.Controllers
         }
 
         [HttpGet("get-room-price-by-price")]
-        public async Task<IActionResult> GetRoomPriceByPrice(double price)
+        public async Task<IActionResult> GetRoomPriceByPrice(double Price)
         {
-            var roomPrices = await roomPriceRepository.GetRoomPriceByPrice(price);
+            var roomPrices = await roomPriceRepository.GetRoomPriceByPrice(Price);
             if (roomPrices == null)
             {
                 return NotFound(new JsonResult(new { title = "Error", message = "Room price isn't exist" }));
