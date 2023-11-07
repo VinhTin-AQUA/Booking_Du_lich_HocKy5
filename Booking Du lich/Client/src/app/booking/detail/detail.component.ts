9efd14b2276ba649469f6294b4cfae7d9de96deb
NguyenNgocTieuThuu <Nguyennngoctieuthu2207@gmail.com>
2023-11-07 16:06:24 +0700

    bookroom

--- Booking Du lich/Client/src/app/booking/detail/detail.component.ts
+++ Booking Du lich/Client/src/app/booking/detail/detail.component.ts
@@ -17,10 +17,16 @@ export class DetailComponent {
   cityId: number = -1;
   hotelId: number = -1;
   hotel: Hotel | null = null;
-  hotelImg: ImgShow[] =[]
+  hotelImg: ImgShow[] = []
   imgBase = environment.imgUrl;
-  rooms: Room[] =[];
+  rooms: Room[] = [];
   fileNames: string[] = []
+
+  baseImg = environment.imgUrl;
+
+  //======================================
+  slider: any;
+  defaultTransform: any;
 
   constructor(private activatedRoute: ActivatedRoute,
     private agentService: AgentService,
@@ -37,12 +43,28 @@ export class DetailComponent {
 
     this.getHotel();
     this.getRoomsOfHotel();
+
+    this.slider = document.getElementById("slider");
+    this.defaultTransform = 0
   }
-  
+
+  goNext() {
+    this.defaultTransform = this.defaultTransform - 398;
+    if (Math.abs(this.defaultTransform) >= this.slider.scrollWidth / 1.7) this.defaultTransform = 0;
+    this.slider.style.transform = "translateX(" + this.defaultTransform + "px)";
+  }
+
+  goPrev() {
+
+    if (Math.abs(this.defaultTransform) === 0) this.defaultTransform = 0;
+    else this.defaultTransform = this.defaultTransform + 398;
+    this.slider.style.transform = "translateX(" + this.defaultTransform + "px)";
+  }
+
   private getHotel() {
     this.agentService.getHotelById(this.hotelId).subscribe((h: any) => {
       this.hotel = h.hotel;
-      
+
       for (let img of h.imgNames) {
         const arr = img.split('\\');
         const imgName: string = arr[arr.length - 1];
@@ -53,6 +75,8 @@ export class DetailComponent {
         };
         this.hotelImg.push(imgShow);
       }
+
+      console.log(this.hotelImg);
     })
   }
 
@@ -65,7 +89,7 @@ export class DetailComponent {
       },
       error: (err) => {
         console.log(err);
-        
+
       }
     })
   }
