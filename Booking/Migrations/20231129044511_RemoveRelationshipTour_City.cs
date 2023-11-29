df063bbc12cec2c9db399716053a3da08b1ab8f5
7iendat <datnguyentien0803@gmail.com>
2023-11-29 12:14:05 +0700

    fix some error

---
+++ Booking/Migrations/20231129044511_RemoveRelationshipTour_City.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRelationshipTour_City : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tour_City_CityId",
+                table: "Tour");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tour_CityId",
+                table: "Tour");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Tour");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CityId",
+                table: "Tour",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tour_CityId",
+                table: "Tour",
+                column: "CityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tour_City_CityId",
+                table: "Tour",
+                column: "CityId",
+                principalTable: "City",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Booking/Migrations/20231129051321_UpdateRelationshipSthing.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRelationshipSthing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tour_Category_TourTypeId",
+                table: "Tour");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tour_TourTypeId",
+                table: "Tour");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Tour_TourTypeId",
+                table: "Tour",
+                column: "TourTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tour_Category_TourTypeId",
+                table: "Tour",
+                column: "TourTypeId",
+                principalTable: "Category",
+                principalColumn: "CategoryId");
+        }
+    }
+}
