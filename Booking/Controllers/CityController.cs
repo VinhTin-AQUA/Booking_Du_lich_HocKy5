67321477fad8fd03002e437ff4729cdf6fc59e31
VinhTin-AQUA <tinhovinh@gmail.com>
2023-12-12 16:00:43 +0700

    thêm search gợi ý

---
+++ Booking/Controllers/CityController.cs
@@ -0,0 +1,22 @@
+﻿using Booking.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.Controllers
+{
+	public class CityController : Controller
+	{
+		private readonly ICityRepository cityRepository;
+
+		public CityController(ICityRepository cityRepository)
+        {
+			this.cityRepository = cityRepository;
+		}
+
+		public async Task<IActionResult> GetAllCities()
+		{
+			var cities = await cityRepository.GetAllCities();
+			var cityNames = cities.Select(city => city.Name).ToList();
+			return Json(new { cityNames });
+		}
+    }
+}
