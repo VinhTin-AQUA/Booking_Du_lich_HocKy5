30a3afbc9eff2a0bd88ff0e3c399177f4c726cc2
VinhTin-AQUA <tinhovinh@gmail.com>
2023-12-15 00:19:37 +0700

    thêm dữ liệu mẫu

--- Booking/Controllers/HomeController.cs
+++ Booking/Controllers/HomeController.cs
@@ -229,7 +229,6 @@ namespace Booking.Controllers
 
         [Route("book-tour/{packageId}")]
         [HttpGet]
-
         public async Task<IActionResult> BookTour(int packageId)
         {
             var packagePrice = await _packagePriceRepository.GetPackagePriceByPackageId(packageId);
@@ -246,7 +245,6 @@ namespace Booking.Controllers
 
         [Route("book-tour/{packageId}")]
         [HttpPost]
-
         public async Task<IActionResult> BookTour(int packageId, BookTour model)
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
